bf4e2a86de16203b401a4a864241369e77895a10
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2022-12-05 12:46:42 -0600

    Update day5.py

--- Day5/day5.py
+++ Day5/day5.py
@@ -6,18 +6,17 @@ data = open(path).read().split('\n')
 
 boxes = [[x for x in y[1::4]] for y in data[:8]]
 boxes = np.rot90(boxes, 3)
-boxes = list([list([y for y in ''.join(x).strip()]) for x in boxes])
+boxes = [[y for y in ''.join(x).strip()] for x in boxes]
 
 b = deepcopy(boxes)
 b2 = deepcopy(boxes)
 
 for d in data[10:]:
-    n = list(map(int, re.findall('\d+', d)))
-    m, f, t = n[0], n[1] - 1, n[2] - 1
-    b[t].extend(b[f][-m:][::-1])
-    b[f] = b[f][:-m]
-    b2[t].extend(b2[f][-m:])
-    b2[f] = b2[f][:-m]
+    m, f, t = list(map(int, re.findall('\d+', d)))
+    b[t - 1].extend(b[f][-m:][::-1])
+    b[f - 1] = b[f - 1][:-m]
+    b2[t - 1].extend(b2[f][-m:])
+    b2[f - 1] = b2[f - 1][:-m]
     
 print(''.join([x[-1] for x in b]))
 print(''.join([x[-1] for x in b2]))
